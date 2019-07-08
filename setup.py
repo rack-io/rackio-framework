1f052e0a9d3f2d6cd572d722d5cb78174a17245a
Nelson Carrasquel <carrasquel@outlook.com>
2019-07-08 17:06:42 -0400

    New version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Rackio",
-    version="0.2.2",
+    version="0.2.3",
     author="Nelson Carrasquel",
     author_email="rackio.framework@outlook.com",
     description="A modern Python Framework for microboard automation and control applications development",
