33ee0e3dce11a342504449250dcc8e35598824c6
Nelson Carrasquel <carrasquel@outlook.com>
2019-08-15 09:27:04 -0430

    API restructure and improvements

--- rackio/api/__init__.py
+++ rackio/api/__init__.py
@@ -1,2 +1,10 @@
-from .api import *
+# -*- coding: utf-8 -*-
+"""rackio/api/__init__.py
+
+This module implements all API initializations.
+"""
 from .tags import *
+from .controls import *
+from .alarms import *
+from .events import *
+from .workers import *
