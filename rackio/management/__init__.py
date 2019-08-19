5d98dc68b3bb810d98b509e44b23781df2cdf5e7
Nelson Carrasquel <carrasquel@outlook.com>
2019-08-19 15:44:33 -0400

    Modules for CLI management added

---
+++ rackio/management/__init__.py
@@ -0,0 +1,6 @@
+from .utilities import *
+
+def execute_from_command_line(argv=None):
+    """Run a ManagementUtility."""
+    utility = ManagementUtility(argv)
+    utility.execute()
---
+++ rackio/management/utilities.py
@@ -0,0 +1,9 @@
+class ManagementUtility(object):
+
+    def __init__(self, argv):
+
+        self.argv = argv
+    
+    def execute(self):
+
+        pass
