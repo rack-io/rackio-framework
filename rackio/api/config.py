aa9970140d18736ebe53d1245af533225b20a2dc
Nelson Carrasquel <carrasquel@outlook.com>
2019-12-16 15:54:53 -0500

    Minified versions used, complete versions deleted

--- rackio/api/config.py
+++ rackio/api/config.py
@@ -4,21 +4,21 @@
 ## Base Modules
 
 rackio_modules = [
-    {'folder': 'static', 'subfolder': 'js', 'module': 'jquery.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-touch.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-cookies.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-animate.js'}, 
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-aria.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-messages.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-sanitize.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-ui-router.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'jquery.min.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular.min.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-touch.min.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-cookies.min.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-animate.min.js'}, 
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-aria.min.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-messages.min.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-sanitize.min.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-ui-router.min.js'},
     {'folder': 'static', 'subfolder': 'js', 'module': 'd3.min.js'},
     {'folder': 'static', 'subfolder': 'js', 'module': 'nv.d3.min.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-nvd3.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-material.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-nvd3.min.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'angular-material.min.js'},
     {'folder': 'static', 'subfolder': 'js', 'module': 'angular-mocks.js'},
-    {'folder': 'static', 'subfolder': 'js', 'module': 'md-data-table.js'},
+    {'folder': 'static', 'subfolder': 'js', 'module': 'md-data-table.min.js'},
     {'folder': 'static', 'subfolder': 'js', 'module': 'svg-assets-cache.js'},
     
 ]
