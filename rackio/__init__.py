27ff4f618ee545f716332018f471272ed8469b89
Nelson Carrasquel <carrasquel@outlook.com>
2020-08-29 05:06:42 -0400

    Version 0.7.0 with Documentation improvements

--- rackio/__init__.py
+++ rackio/__init__.py
@@ -1,5 +1,17 @@
-# -*- coding: utf-8 -*-
-# rackio/__init__.py
+"""
+Automation, Control and IoT Framework for Python
+==================================
+Rackio is a Modern Python Framework for microboard 
+automation and control applications development. 
+
+It aims to provide simple and efficient solutions
+to the most common software architectures found in 
+the control and automation industry.
+
+See https://rackio-framework.readthedocs.io/ for complete documentation.
+"""
+
+__version__ = '0.7.0'
 
 from .core import Rackio
 from .engine import CVTEngine as TagEngine
