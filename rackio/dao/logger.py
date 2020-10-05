a1e88c77687c64452f3f7f6e066a56db819603bf
crivero7 <cdrr.rivero@gmail.com>
2020-10-05 12:47:45 -0400

    Logger DAO

---
+++ rackio/dao/logger.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""rackio/dao/logger.py
+
+This module implements Logger Data Objects Access.
+"""
+from .core import RackioDAO
+
+class LoggerDAO(RackioDAO):
+
+    def get_all(self):
+
+        app = self.get_app()
+
+        return app.get_dbtags()
+        
