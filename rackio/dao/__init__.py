aee6abec8ccc923cb47c22b2c7ad3643debbddb3
crivero7 <cdrr.rivero@gmail.com>
2020-10-05 12:50:05 -0400

    dao's added to package init

--- rackio/dao/__init__.py
+++ rackio/dao/__init__.py
@@ -3,3 +3,8 @@
 
 This module implements all Rackio Data Objects Access.
 """
+
+from .tags import TagsDAO
+from .logger import LoggerDAO
+from .controls import ControlDAO
+from .alarms import AlarmDAO
