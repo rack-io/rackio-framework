c0bcd9a73038d371239b9c0c302a5148e872e0f6
Nelson Carrasquel <carrasquel@outlook.com>
2020-10-13 09:59:22 -0400

    Logger query stripped in waveform y trend

--- rackio/logger/query.py
+++ rackio/logger/query.py
@@ -34,7 +34,7 @@ class QueryLogger:
         
         return trend.period
 
-    def query(self, tag, start, stop):
+    def query_waveform(self, tag, start, stop):
 
         _query = TagTrend.select().order_by(TagTrend.start)
         trend = _query.where(TagTrend.name == tag).get()
@@ -59,7 +59,26 @@ class QueryLogger:
 
         return result
 
-    def query_last(self, tag, seconds=None, values=None):
+    def query_trend(self, tag, start, stop):
+
+        _query = TagTrend.select().order_by(TagTrend.start)
+        trend = _query.where(TagTrend.name == tag).get()
+        
+        start = datetime.strptime(start, DATETIME_FORMAT)
+        stop = datetime.strptime(stop, DATETIME_FORMAT)
+        
+        _query = trend.values.select()
+        values = _query.where((TagValue.timestamp > start) & (TagValue.timestamp < stop))
+        
+        result = dict()
+
+        values = [{"x": value.timestamp.strftime(DATETIME_FORMAT), "y": value.value} for value in values]
+
+        result["values"] = values
+
+        return result
+
+    def query_last(self, tag, seconds=None, values=None, waveform=False):
 
         if seconds:
 
@@ -69,7 +88,11 @@ class QueryLogger:
             start = start.strftime(DATETIME_FORMAT)
             stop = stop.strftime(DATETIME_FORMAT)
 
-            return self.query(tag, start, stop)
+            if waveform:
+
+                return self.query_waveform(tag, start, stop)
+
+            return self.query_trend(tag, start, stop)
 
         if values:
 
@@ -86,17 +109,15 @@ class QueryLogger:
             except Exception as e:
                 tag_values = tag_values[:]
 
-            t0 = tag_values[0].timestamp.strftime(DATETIME_FORMAT)
-            tag_values = [value.value for value in tag_values]
+            start = tag_values[0].timestamp
+            stop = tag_values[-1].timestamp
 
-            result["t0"] = t0
-            result["dt"] = period
-        
-            result["values"] = tag_values
+            if waveform:
+                return self.query_waveform(tag, start, stop)
+            
+            return self.query_trend(tag, start, stop)
 
-            return result
-
-    def query_first(self, tag, seconds=None, values=None):
+    def query_first(self, tag, seconds=None, values=None, waveform=False):
 
         tag_values = self.get_values(tag)
 
@@ -108,7 +129,10 @@ class QueryLogger:
             start = start.strftime(DATETIME_FORMAT)
             stop = stop.strftime(DATETIME_FORMAT)
 
-            return self.query(tag, start, stop)
+            if waveform:
+                return self.query_waveform(tag, start, stop)
+            
+            return self.query_trend(tag, start, stop)
 
         if values:
 
@@ -126,9 +150,10 @@ class QueryLogger:
             except Exception as e:
                 tag_values = tag_values[:]
                 
-            result["t0"] = t0
-            result["dt"] = period
-        
-            result["values"] = tag_values
+            start = tag_values[0].timestamp
+            stop = tag_values[-1].timestamp
 
-            return result
+            if waveform:
+                return self.query_waveform(tag, start, stop)
+            
+            return self.query_trend(tag, start, stop)
