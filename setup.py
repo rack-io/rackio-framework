0770d61b57052f1ae7d5ea777b24f0e09067fec9
Nelson Carrasquel <carrasquel@outlook.com>
2021-04-06 20:08:14 -0300

    Minor fixes to v1.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Rackio",
-    version="1.0.3",
+    version="1.0.4",
     author="Nelson Carrasquel",
     author_email="rackio.framework@outlook.com",
     description="A modern Python Framework for microboard automation and control applications development",
@@ -18,7 +18,7 @@ setuptools.setup(
     include_package_data=True,
     packages=setuptools.find_packages(),
     install_requires=[
-        'falcon',
+        'falcon==2.0.0',
         'falcon-multipart',
         'falcon-auth',
         'falcon-cors',
