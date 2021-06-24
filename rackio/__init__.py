6ef94550a5ff45ef8d50980d312e4d47668e2e23
Nelson Carrasquel <carrasquel@outlook.com>
2021-06-23 19:25:34 -0500

    version 1.0.5

--- rackio/__init__.py
+++ rackio/__init__.py
@@ -11,7 +11,7 @@ the control and automation industry.
 See https://rackio-framework.readthedocs.io/ for complete documentation.
 """
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 from .core import Rackio
 from .engine import CVTEngine as TagEngine
