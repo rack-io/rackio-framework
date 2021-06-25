38e94f10c4a5f06a33e73e25b0519109a885a76a
Nelson Carrasquel <carrasquel@outlook.com>
2021-06-25 01:16:57 -0500

    base for run command

--- rackio/cli.py
+++ rackio/cli.py
@@ -1,3 +1,4 @@
+import os
 import click
 
 from dotenv import load_dotenv
@@ -11,3 +12,37 @@ def rackio_cli(keywords):
     if keywords == "serve":
 
         load_dotenv()
+
+        rackio_app = os.getenv("RACKIO_APP")
+
+        if not rackio_app:
+
+            try:
+                module = __import__("app")
+            except:
+                pass
+
+            try:
+                module = __import__("wsgi")
+            except:
+                pass
+        else:
+
+            try:
+                module = __import__(rackio_app)
+            except Exception as e:
+                print(e)
+                return
+        
+        attrs = dir(module)
+
+        if "app" in attrs:
+            app = module.app
+        elif "application" in attrs:
+            app = module.application
+        elif "create_app" in attrs:
+            app = module.create_app()
+        elif "make_app" in attrs:
+            app = module.make_app()
+        
+        app.run()
