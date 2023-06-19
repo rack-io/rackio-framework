fb805b32160d6b94137b5d69ec73705577a24f80
Nelson Carrasquel <carrasquel@outlook.com>
2023-06-18 20:59:39 -0400

    black appied on connection.py

--- rackio/connection.py
+++ rackio/connection.py
@@ -9,18 +9,14 @@ READ = "read"
 
 
 class TagBinding:
-
     def __init__(self, local_tag, remote_tag, direction):
-
         self.local_tag = local_tag
         self.remote_tag = remote_tag
         self.direction = direction
 
 
 class RackioBinding:
-
     def __init__(self, name, host_ip, host_port):
-
         self.name = name
         self.host_ip = host_ip
         self.host_port = host_port
@@ -30,10 +26,7 @@ class RackioBinding:
         return self.host_ip, self.host_port
 
     def append_binding(self, tag_binding):
-
         self.tag_bindings.append(tag_binding)
 
     def get_bindings(self):
-
         return self.tag_bindings
-    
