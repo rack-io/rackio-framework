5b23e7793a6b110de19893cc63c9cdf84a26ec52
Nelson Carrasquel <carrasquel@outlook.com>
2023-06-18 21:13:26 -0400

    black appied on /api/waveforms.py

--- rackio/api/waveforms.py
+++ rackio/api/waveforms.py
@@ -14,17 +14,14 @@ from ..managers.auth import SYSTEM_ROLE, ADMIN_ROLE, VISITOR_ROLE
 
 
 class BaseResource(RackioResource):
-
     dao = TagsDAO()
 
 
 class WaveformResource(BaseResource):
-
     @authorize([SYSTEM_ROLE, ADMIN_ROLE, VISITOR_ROLE])
     def on_post(self, req, resp, tag_id):
-
-        tstart = req.media.get('tstart')
-        tstop = req.media.get('tstop')
+        tstart = req.media.get("tstart")
+        tstop = req.media.get("tstop")
 
         doc = self.dao.get_waveform(tag_id, tstart, tstop)
 
@@ -32,15 +29,13 @@ class WaveformResource(BaseResource):
 
 
 class WaveformCollectionResource(BaseResource):
-
     @authorize([SYSTEM_ROLE, ADMIN_ROLE, VISITOR_ROLE])
     def on_post(self, req, resp):
+        tags = req.media.get("tags")
 
-        tags = req.media.get('tags')
+        tstart = req.media.get("tstart")
+        tstop = req.media.get("tstop")
 
-        tstart = req.media.get('tstart')
-        tstop = req.media.get('tstop')
-    
         result = self.dao.get_waveforms(tags, tstart, tstop)
 
         resp.body = json.dumps(result, ensure_ascii=False)
