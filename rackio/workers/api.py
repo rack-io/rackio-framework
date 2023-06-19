e5cdf5b4e619a02fbea169506ffc64e1d9f3808e
Nelson Carrasquel <carrasquel@outlook.com>
2023-06-18 21:32:32 -0400

    black appied on /workers/api.py

--- rackio/workers/api.py
+++ rackio/workers/api.py
@@ -12,9 +12,7 @@ from .handlers import CustomWSGIRequestHandler
 
 
 class APIWorker(BaseWorker):
-
     def __init__(self, manager, port=8000, mode="development"):
-
         super(APIWorker, self).__init__()
 
         self._manager = manager
@@ -22,14 +20,17 @@ class APIWorker(BaseWorker):
         self._mode = mode
 
     def run(self):
-
         app = self._manager.app
         app.set_middleware()
 
         if self._mode == "development":
-            with make_server('', self._port, app, handler_class=CustomWSGIRequestHandler) as httpd:
-                logging.info('Development mode serving on port {}...'.format(self._port))
+            with make_server(
+                "", self._port, app, handler_class=CustomWSGIRequestHandler
+            ) as httpd:
+                logging.info(
+                    "Development mode serving on port {}...".format(self._port)
+                )
                 httpd.serve_forever()
         else:
-            logging.info('Production mode serving on port {}...'.format(self._port))
-            serve(app, host='0.0.0.0', port=self._port)
+            logging.info("Production mode serving on port {}...".format(self._port))
+            serve(app, host="0.0.0.0", port=self._port)
