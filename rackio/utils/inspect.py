33b283d367d535475243be49ce5db5114a75c797
Nelson Carrasquel <carrasquel@outlook.com>
2023-06-18 21:29:05 -0400

    black appied on /utils/inspect.py

--- rackio/utils/inspect.py
+++ rackio/utils/inspect.py
@@ -9,14 +9,13 @@ import types
 
 
 def log_detailed(e, message):
-    
     logging.error(message)
     logging.error(e, exc_info=True)
 
-def is_function(f):
 
+def is_function(f):
     return isinstance(f, types.FunctionType)
 
-def is_class(cls):
 
+def is_class(cls):
     return inspect.isclass(cls)
