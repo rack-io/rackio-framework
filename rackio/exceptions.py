b61c493ca81956091af77032778c5db4f5f6184b
Nelson Carrasquel <carrasquel@outlook.com>
2023-06-18 21:02:09 -0400

    black appied on exceptions.py

--- rackio/exceptions.py
+++ rackio/exceptions.py
@@ -4,26 +4,32 @@
 This module defines all exceptions handle by Rackio.
 """
 
+
 class RackioError(Exception):
     """Base class for other exceptions"""
+
     pass
 
 
 class InvalidTagNameError(RackioError):
     """Raised when an invalid tag name is defined"""
+
     pass
 
 
 class TagNotFoundError(RackioError):
     """Raised when a Tag Name was not found in repository"""
+
     pass
 
 
 class InvalidTagTypeError(RackioError):
     """Raised when a Tag is assigned a different type object"""
+
     pass
 
 
 class WorkerError(RackioError):
     """Raised when an error occurs in a Continous Worker"""
+
     pass
