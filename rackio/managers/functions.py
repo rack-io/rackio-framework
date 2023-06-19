5fc5361fd870a421f34cac5633ce24328671d146
Nelson Carrasquel <carrasquel@outlook.com>
2023-06-18 21:26:11 -0400

    black appied on /managers/functions.py

--- rackio/managers/functions.py
+++ rackio/managers/functions.py
@@ -8,32 +8,27 @@ import queue
 from ..engine import CVTEngine
 from ..models import TagObserver
 
+
 class FunctionManager:
-
     def __init__(self):
-
         self._tags = dict()
         self._tag_queue = queue.Queue()
 
     def get_queue(self):
-
         return self._tag_queue
 
     def append_function(self, tag, function):
-
         try:
             self._tags[tag].append(function)
         except Exception as e:
             self._tags[tag] = [function]
 
     def summary(self):
-
         result = dict()
 
         length = 0
 
         for functions in self._tags.values():
-
             length += len(functions)
 
         result["length"] = length
@@ -41,17 +36,14 @@ class FunctionManager:
         return result
 
     def attach_all(self):
-
         engine = CVTEngine()
 
         for _tag in self._tags:
-
             observer = TagObserver(self._tag_queue)
-            
-            engine.attach(_tag, observer)
-        
-    def execute(self, tag):
 
+            engine.attach(_tag, observer)
+
+    def execute(self, tag):
         try:
             for _function in self._tags[tag]:
                 _function()
