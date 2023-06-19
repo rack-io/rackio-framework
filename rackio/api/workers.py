9d3c4c29e2099a20a35ea88b1380a0a1b7659f23
Nelson Carrasquel <carrasquel@outlook.com>
2023-06-18 21:13:42 -0400

    black appied on /api/workers.py

--- rackio/api/workers.py
+++ rackio/api/workers.py
@@ -15,10 +15,8 @@ from ..managers.auth import SYSTEM_ROLE, ADMIN_ROLE, VISITOR_ROLE
 
 
 class ContinousWorkerResource(RackioResource):
-
     @authorize([SYSTEM_ROLE, ADMIN_ROLE, VISITOR_ROLE])
     def on_get(self, req, resp, worker_name):
-
         app = self.get_app()
         manager = app.get_manager("alarm")
 
@@ -35,8 +33,7 @@ class ContinousWorkerResource(RackioResource):
 
     @authorize([SYSTEM_ROLE, ADMIN_ROLE])
     def on_post(self, req, resp, alarm_name):
-        
-        action = req.media.get('action')
+        action = req.media.get("action")
 
         app = self.get_app()
         manager = app.get_manager("alarm")
@@ -45,13 +42,12 @@ class ContinousWorkerResource(RackioResource):
 
         if alarm:
             if action == "Acknowledge":
-
                 alarm.acknowledge()
 
             doc = alarm.serialize()
 
             resp.body = json.dumps(doc, ensure_ascii=False)
-            
+
             resp.status = status_code.HTTP_200
         else:
             resp.status = status_code.HTTP_NOT_FOUND
